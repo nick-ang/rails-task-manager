c177bf91961c2d76c808be52f891e9123f1b16f8
Nick Ang <nicholas_ang@outlook.com>
2021-08-24 15:18:58 +1000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
   get "tasks", to: "tasks#index"
   get "new", to: "tasks#new"
   get "task/:id", to: "tasks#show", as: :task
