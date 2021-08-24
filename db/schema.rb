dffe8053223e468fec83cf8f3fcd6a12d6ac2340
Nick Ang <nicholas_ang@outlook.com>
2021-08-24 14:57:51 +1000

    init

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_08_24_015103) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
@@ -18,4 +22,5 @@ ActiveRecord::Schema.define(version: 2021_08_24_015103) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
